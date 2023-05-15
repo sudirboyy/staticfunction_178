f55470974b0765fed650676a83f63b4757770a58
sudirboyy <sudirboyy@gmail.com>
2023-05-15 18:38:02 +0700

    2

--- Chainfunction_178/Chainfunction_178.cpp
+++ Chainfunction_178/Chainfunction_178.cpp
@@ -1,8 +1,21 @@
-// Chainfunction_178.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+class buku
+{
+	string judul;
 
+public:
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+
+} bukunya;
 int main()
 {
     std::cout << "Hello World!\n";
