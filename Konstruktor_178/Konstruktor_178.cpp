662eb308ee5760ff11e047ef6cca1c8d84993eb6
sudirboyy <sudirboyy@gmail.com>
2023-05-15 18:49:03 +0700

    3

--- Konstruktor_178/Konstruktor_178.cpp
+++ Konstruktor_178/Konstruktor_178.cpp
@@ -14,6 +14,23 @@ public:
 	void cetak();
 };
 
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
 int main()
 {
     std::cout << "Hello World!\n";
