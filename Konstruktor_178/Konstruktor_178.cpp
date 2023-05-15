a9473afa4cc7ecad6a9e1bcd8a5f34c114013747
sudirboyy <sudirboyy@gmail.com>
2023-05-15 18:49:18 +0700

    4

--- Konstruktor_178/Konstruktor_178.cpp
+++ Konstruktor_178/Konstruktor_178.cpp
@@ -31,6 +31,10 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
 int main()
 {
     std::cout << "Hello World!\n";
