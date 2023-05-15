7d5809810c8553b9ac486e124250686a47e8e5d3
sudirboyy <sudirboyy@gmail.com>
2023-05-15 15:27:32 +0700

    1

--- staticfunction_178/staticfunction_178.cpp
+++ staticfunction_178/staticfunction_178.cpp
@@ -1,7 +1,6 @@
-// staticfunction_178.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
