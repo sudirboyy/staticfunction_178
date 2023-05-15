66c8c004525b2fab648525a28c261b9329457a2e
sudirboyy <sudirboyy@gmail.com>
2023-05-15 15:29:48 +0700

    2

--- staticfunction_178/staticfunction_178.cpp
+++ staticfunction_178/staticfunction_178.cpp
@@ -2,6 +2,16 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
 int main()
 {
     std::cout << "Hello World!\n";
