96ab26c84b5b867b1296e48d55982b139f99bd82
sudirboyy <sudirboyy@gmail.com>
2023-05-15 18:47:11 +0700

    2

--- Konstruktor_178/Konstruktor_178.cpp
+++ Konstruktor_178/Konstruktor_178.cpp
@@ -1,7 +1,18 @@
-// Konstruktor_178.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
 
 int main()
 {
