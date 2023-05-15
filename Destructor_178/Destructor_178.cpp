966c8e3a3cb14944249fc534da6854d9ea1031ab
sudirboyy <sudirboyy@gmail.com>
2023-05-15 18:46:14 +0700

    6

--- Destructor_178/Destructor_178.cpp
+++ Destructor_178/Destructor_178.cpp
@@ -34,7 +34,10 @@ void angka::isiData() {
 	}
 	cout << endl;
 }
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+	angka belajarcpp(3); //Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+	delete ptrBelajarcpp; //Destructor Dipanggil
+
+	return 0;
+}//Destructor Dipanggil
